06fc776938353067e3d4696deff18cf989619fab
Setya Carina Rianti <145129875+setyacarinaa@users.noreply.github.com>
2023-12-01 21:41:26 +0700

    Add files via upload

---
+++ jobsheet 1/soruce code/jobsheet 1.cpp
@@ -0,0 +1,14 @@
+/* 	nama tugas : Tugas Pertama
+	nama programer : Setya Carina Rianti / 23343054
+	Kasus Program : Membuat Program Hello World
+*/
+#include <stdio.h>
+
+int main(){
+	
+	printf("Hello World!");
+	printf("\n\n===============================\n\n");
+	
+	return 0;
+	
+}
